8529dd5f3d33d21f5753fdf05c97dc62d858341d
sarahcruzz <sarah.cruz0492@gmail.com>
2024-08-27 16:46:26 -0300

    :construction: em progresso

--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -1,18 +1,13 @@
 package com.picpaysimplificado.repositories;
 
 import com.picpaysimplificado.domain.user.User;
-import com.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.math.BigDecimal;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,34 +16,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRepositoryTest {
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
     EntityManager entityManager;
 
     @Test
-    @DisplayName("Should get User successfully from DB")
-    void findUserByDocumentCase1() {
-        String document = "99999999901";
-        UserDTO data = new UserDTO("Fernanda", "Teste", document, new BigDecimal(10), "test@gmail.com", "44444", UserType.COMMON);
-        this.createUser(data);
-
-        Optional<User> result = this.userRepository.findUserByDocument(document);
-
-        assertThat(result.isPresent()).isTrue();
+    void findUserByDocument() {
     }
 
-    @Test
-    @DisplayName("Should not get User from DB when user not exists")
-    void findUserByDocumentCase2() {
-        String document = "99999999901";
-
-        Optional<User> result = this.userRepository.findUserByDocument(document);
-
-        assertThat(result.isEmpty()).isTrue();
-    }
-
-    private User createUser(UserDTO data){
+    // método que está sendo criado apenas como auxiliar para o método de teste do findUserByDocument   a
+    private User create_user(UserDTO data){
         User newUser = new User(data);
         this.entityManager.persist(newUser);
         return newUser;
